7456f1cb31e1c08d2ea79b142d48e987275105ab
Ryan de Gruyter <degruyter.ryan@gmail.com>
2017-04-08 21:14:56 +0200

    wd

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,7 +4,6 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 11000,
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
@@ -14,24 +13,5 @@ exports.config = {
     'browserstack.user': 'ryandegruyter1',
     'browserstack.key': 'MHDuSpLiEwcHTXp3vDb7',
     'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  useAllAngular2AppRoots: true,
-  beforeLaunch: function() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-
-    require('connect')().use(require('serve-static')('www')).listen(4200);
-  },
-  onPrepare() {
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
